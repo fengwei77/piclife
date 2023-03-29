4063cf0f919158bc9578b8c628bd9f8de81d8a5d
luke <fengwei543@gmail.com>
2023-03-29 23:24:02 +0800

    切版全頁面

--- assets/js/lihsi.js
+++ assets/js/lihsi.js
@@ -4,13 +4,19 @@
 * @date    2020-10-15 20:37:10
 * @version $Id$
 */
-// if the image in the window of browser when the page is loaded, show that image
-// 
+
+//when online change to false
+var environmentDevelop = true;
+
 // if the image in the window of browser when the page is loaded, show that image
 var currentWidth = $(window).width();
 
 function showWidth()
 {
+    if(!environmentDevelop){
+        $("#test-width").hide();
+    }
+
     currentWidth = $(window).width();
     currentHeight = $(window).height()
 
